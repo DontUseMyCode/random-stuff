9cd1c9d40cb8bf98d90129ce907497af7f9d0475
sasa <sasa@dhigroup.com>
2023-11-28 22:13:58 +0100

    void

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-let x = 0;
 
-const increment = () => {
-
-    x++;
+const top_nav_buttons_expanded = {
+    "top-nav-item-tutorials": false,
+    "top-nav-item-exercises": false,
+    "top-nav-item-certificates": false,
+    "top-nav-item-services": false
 }
 
-const on_x_change = ()=> {
-    const x_container = document.getElementById("x-container");
-    x_container.innerText = x.toString();
+for (const id of Object.keys(top_nav_buttons_expanded))
+{
+    const element = document.getElementById(id)
+    element.addEventListener(("click"), () => {
+        const expanded = !top_nav_buttons_expanded[id]
+        top_nav_buttons_expanded[id] = expanded
+    
+        if (expanded) {
+            element.classList.remove("arrow-down")
+            element.classList.add("arrow-up")
+            document.getElementById("nav-menu").hidden=false
+        }
+        else {
+            element.classList.remove("arrow-up")
+            element.classList.add("arrow-down")
+            document.getElementById("nav-menu").hidden=true
 
+        }
+    })
 }
-
-
-const increment_button = document.getElementById("increment-button");
-
-increment_button.addEventListener(("click"), ()=> {
-
-    increment();
-    on_x_change();
-    console.log(x);
-})
